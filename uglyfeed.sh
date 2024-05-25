5f22e0a8ada488da01504b71efda6e9f31b1f167
fab <fabrizio.salmi@gmail.com>
2024-05-25 22:40:45 +0200

    Update uglyfeed.sh
    small workaround.. proper fix will be released further :)

--- uglyfeed.sh
+++ uglyfeed.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # replace llm_processor.py with llm_processor_openai.py to use OpenAI models
+# 
+# required:
+# for llm_processor.py         url (ex: http://192.168.100.55:11434/api/chat) and model
+# for llm_processor_openai.py  api key and model
 
 # Script configuration (easily maintainable)
 scripts=(
     "main.py"
-    "llm_processor.py"
+    "llm_processor.py" # this will not work anymore, a proper solution is coming, in the meanwhile.. check examples below
+#   "llm_processor.py --api_url http://192.168.100.55:11434/api/chat --model llama3
+#   "llm_processor_openai.py --api_key sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxx --model gpt4o
     "json2rss.py"
     "serve.py"
 )
